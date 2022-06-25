ee63040c3d3a735043087ee79435e6e21788a9b0
wonglar <wonglar@163.com>
2022-06-25 08:20:44 +0800

    单链表

--- 03单链表/03单链表/Demo.c
+++ 03单链表/03单链表/Demo.c
@@ -47,23 +47,24 @@ struct ListNode* removeElement(struct ListNode* head, int val)
 }
 
 
-int main()
-{
-
-	struct ListNode* n1 = (struct ListNode*)malloc(sizeof(struct ListNode));
-	struct ListNode* n2 = (struct ListNode*)malloc(sizeof(struct ListNode));
-	struct ListNode* n3 = (struct ListNode*)malloc(sizeof(struct ListNode));
-	struct ListNode* n4 = (struct ListNode*)malloc(sizeof(struct ListNode));
-	n1->val = 7;
-	n2->val = 7;
-	n3->val = 7;
-	n4->val = 7;
-	n1->next = n2;
-	n2->next = n3;
-	n3->next = n4;
-	n4->next = NULL;
-
-	struct ListNode* head = removeElement(n1, 7);
-
-	return 0;
-}
+//int main()
+//{
+//
+//	struct ListNode* n1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+//	struct ListNode* n2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+//	struct ListNode* n3 = (struct ListNode*)malloc(sizeof(struct ListNode));
+//	struct ListNode* n4 = (struct ListNode*)malloc(sizeof(struct ListNode));
+//
+//	n1->val = 7;
+//	n2->val = 7;
+//	n3->val = 7;
+//	n4->val = 7;
+//	n1->next = n2;
+//	n2->next = n3;
+//	n3->next = n4;
+//	n4->next = NULL;
+//
+//	struct ListNode* head = removeElement(n1, 7);
+//
+//	return 0;
+//}
